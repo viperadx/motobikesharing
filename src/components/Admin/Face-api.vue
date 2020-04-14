f47657a83ca1b185655a6637786bc0038740a388
viperadx <alexandru.radu1994@gmail.com>
2020-04-14 22:48:03 +0300

    updated face api
    - capture-> submit pentru prima poza
    - capture-> submit pentru a doua poza
    - verify
    
    la verify returneaza in consola rezultatul

--- src/components/Admin/Face-api.vue
+++ src/components/Admin/Face-api.vue
@@ -12,6 +12,9 @@
         autoplay
       ></video>
     </div>
+    <v-btn @click="faceCapture()">CApture</v-btn>
+    <v-btn @click="faceDetect()">SUBMIT</v-btn>
+    <v-btn @click="faceVerify()">Verify</v-btn>
   </div>
 </template>
 
@@ -30,11 +33,89 @@ export default {
       video: {},
       canvas: {},
       captures: [],
-      testTimer: ""
+      faceId: []
     };
   },
-  computed: {},
+
+  mounted() {
+    //Start the PC front camera and display real-time video on the video tag
+    this.video = this.$refs.video;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ audio: false, video: true })
+        .then((stream) => {
+          this.video.srcObject = stream;
+          this.video.play();
+        });
+    }
+    console.log(this.$refs.canvas);
+    this.canvas = this.$refs.canvas;
+  },
+
   methods: {
+    faceCapture() {
+      this.canvas.getContext("2d").drawImage(this.video, 0, 0, 400, 300); //Store the captured image in the "captures" array
+      this.captures.push(this.canvas.toDataURL("image/png"));
+    },
+    faceDetect() {
+      let subscriptionKey = "51fc2876d96a42e19d922c448dc19990"; //microsoft face api key
+      let uriBase =
+        "https://northeurope.api.cognitive.microsoft.com/face/v1.0/detect";
+
+      //Convert the format of the image added at the end of the array and assign it to the imgURL format
+      const imgURL = this.makeblob(this.captures[this.captures.length - 1]);
+      //Send imgURL image to Face API
+      console.log(imgURL);
+      Axios.post(
+        uriBase +
+          "?returnFaceId=true",
+        imgURL,
+        {
+          headers: {
+            "Content-Type": "application/octet-stream",
+            "Ocp-Apim-Subscription-Key": subscriptionKey,
+          },
+        }
+      )
+        .then((response) => {
+          console.log(response.data[0].faceId);
+          this.faceId.push(response.data[0].faceId)
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+    faceVerify () {
+      let subscriptionKey = "51fc2876d96a42e19d922c448dc19990"; //microsoft face api key
+      let uriBase =
+        "https://northeurope.api.cognitive.microsoft.com/face/v1.0/verify";
+
+      //Convert the format of the image added at the end of the array and assign it to the imgURL format
+      const faceId = {
+        faceId1: this.faceId[0],
+        faceId2: this.faceId[1]
+        }
+        console.log(faceId)
+        console.log(this.faceId[0])
+        console.log(this.faceId[1])
+      Axios.post(
+        uriBase,
+        faceId,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": subscriptionKey,
+          },
+        }
+      )
+        .then((response) => {
+          console.log(response.data.isIdentical)
+          console.log(response.data.confidence)
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
     makeblob: function(dataURL) {
       let BASE64_MARKER = ";base64,";
       if (dataURL.indexOf(BASE64_MARKER) == -1) {
@@ -52,54 +133,11 @@ export default {
         uInt8Array[i] = raw.charCodeAt(i);
       }
       return new Blob([uInt8Array], { type: contentType });
-    }
-  },
-  created() {},
-  mounted() {
-    //Start the PC front camera and display real-time video on the video tag
-    this.video = this.$refs.video;
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ audio: false, video: true })
-        .then(stream => {
-          this.video.srcObject = stream;
-          this.video.play();
-        });
-    }
-    console.log(this.$refs.canvas);
-
-    this.canvas = this.$refs.canvas;
-    this.testTimer = setInterval(() => {
-      // console.log(this.$refs.canvas)
-      this.canvas.getContext("2d").drawImage(this.video, 0, 0, 400, 240); //Store the captured image in the "captures" array
-      this.captures.push(this.canvas.toDataURL("image/png"));
-      let subscriptionKey = "51fc2876d96a42e19d922c448dc19990"; //microsoft face api key
-      let uriBase =
-        "https://northeurope.api.cognitive.microsoft.com/face/v1.0/verify";
-
-      //Convert the format of the image added at the end of the array and assign it to the imgURL format
-      const imgURL = this.makeblob(this.captures[this.captures.length - 1]);
-      //Send imgURL image to Face API
-      console.log(imgURL);
-      Axios.post(
-        uriBase +
-          // "?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,emotion",
-          imgURL,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "Ocp-Apim-Subscription-Key": subscriptionKey
-          }
-        }
-      )
-        .then(response => {
-          // console.log(response.data[0].faceAttributes.emotion);
-          console.log(response);
-        })
-        .catch(error => {
-          console.log(error.response);
-        });
-    }, 5000);
+    },
   }
 };
 </script>
+
+
+
+
